cea150af3aad2596f32dfa999541f35b61be6a49
caueguerra <>
2008-05-19 14:18:31 +0000

    arrumando grafico de raytracing

--- acmus-project/src/acmus/tools/RayTracing.java
+++ acmus-project/src/acmus/tools/RayTracing.java
@@ -68,20 +68,22 @@ public class RayTracing extends Composite {
 
 		public RoomSizeModifyListener(Spinner roomAxis, Spinner sourceAxis,
 				Spinner receiverAxis) {
-					this.roomAxis = roomAxis;
-					this.sourceAxis = sourceAxis;
-					this.receiverAxis = receiverAxis;
+			this.roomAxis = roomAxis;
+			this.sourceAxis = sourceAxis;
+			this.receiverAxis = receiverAxis;
 		}
 
-		@Override
-		public void modifyText(ModifyEvent e){
-			receiverAxis.setMaximum(roomAxis.getSelection());
-			sourceAxis.setMaximum(roomAxis.getSelection());
+		public void modifyText(ModifyEvent e) {
+			this.receiverAxis.setMaximum(this.roomAxis.getSelection());
+			this.sourceAxis.setMaximum(this.roomAxis.getSelection());
 			int min = Integer.MAX_VALUE;
-			if (min > width.getSelection() / 2) min = width.getSelection() / 2;
-			if (min > length.getSelection() / 2) min = length.getSelection() / 2;
-			if (min > height.getSelection() / 2) min = height.getSelection() / 2;
-			radius.setMaximum(min);
+			if (min > RayTracing.this.width.getSelection() / 2)
+				min = RayTracing.this.width.getSelection() / 2;
+			if (min > RayTracing.this.length.getSelection() / 2)
+				min = RayTracing.this.length.getSelection() / 2;
+			if (min > RayTracing.this.height.getSelection() / 2)
+				min = RayTracing.this.height.getSelection() / 2;
+			RayTracing.this.radius.setMaximum(min);
 		}
 	}
 
@@ -136,163 +138,166 @@ public class RayTracing extends Composite {
 		GridData wallsGrid = new GridData(GridData.FILL_HORIZONTAL);
 		wallsGrid.horizontalSpan = 10;
 		walls.setLayoutData(wallsGrid);
-		
-		label = new Label(walls, SWT.None);
-		label.setText("Room Width: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
 
-		width = new Spinner(walls, SWT.None);
-		setSpinner(width, 2, 10000, 100);
-		setGridData(width, SWT.LEAD, SWT.CENTER, 1, 40);
-		
-		label = new Label(walls, SWT.NONE);
-		label.setText("Room Length: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(walls, SWT.None);
+		this.label.setText("Room Width: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		length = new Spinner(walls, SWT.None);
-		setSpinner(length, 2, 10000, 100);
-		setGridData(length, SWT.LEAD, SWT.CENTER, 1, 40);
+		this.width = new Spinner(walls, SWT.None);
+		setSpinner(this.width, 2, 10000, 100);
+		setGridData(this.width, SWT.LEAD, SWT.CENTER, 1, 40);
 
-		label = new Label(walls, SWT.NONE);
-		label.setText("Room Height: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(walls, SWT.NONE);
+		this.label.setText("Room Length: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		height = new Spinner(walls, SWT.None);
-		setSpinner(height, 2, 10000, 100);
-		setGridData(height, SWT.LEAD, SWT.CENTER, 1, 40);
-		
-		label = new Label(this, SWT.LEAD);
-		label.setText("Impulsive response (Hz): ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.length = new Spinner(walls, SWT.None);
+		setSpinner(this.length, 2, 10000, 100);
+		setGridData(this.length, SWT.LEAD, SWT.CENTER, 1, 40);
 
-		respostaImpulsivaText = new Text(this, SWT.NONE);
-		respostaImpulsivaText.setFocus();
-		setGridData(respostaImpulsivaText, SWT.LEAD, SWT.CENTER, 1, 40);
-		
-		label = new Label(this, SWT.NONE);
-		label.setText("Source position: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(walls, SWT.NONE);
+		this.label.setText("Room Height: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		sourceX = new Spinner(this, SWT.NONE);
-		setSpinner(sourceX, 2, 100, 0);
-		setGridData(sourceX, SWT.LEAD, SWT.CENTER, 1);
+		this.height = new Spinner(walls, SWT.None);
+		setSpinner(this.height, 2, 10000, 100);
+		setGridData(this.height, SWT.LEAD, SWT.CENTER, 1, 40);
 
-		sourceY = new Spinner(this, SWT.NONE);
-		setSpinner(sourceY, 2, 100, 0);
-		setGridData(sourceY, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(this, SWT.LEAD);
+		this.label.setText("Impulsive response (Hz): ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		sourceZ = new Spinner(this, SWT.NONE);
-		setSpinner(sourceZ, 2, 100, 0);
-		setGridData(sourceZ, SWT.LEAD, SWT.CENTER, 1);
+		this.respostaImpulsivaText = new Text(this, SWT.NONE);
+		this.respostaImpulsivaText.setFocus();
+		setGridData(this.respostaImpulsivaText, SWT.LEAD, SWT.CENTER, 1, 40);
+
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("Source position: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
+
+		this.sourceX = new Spinner(this, SWT.NONE);
+		setSpinner(this.sourceX, 2, 100, 0);
+		setGridData(this.sourceX, SWT.LEAD, SWT.CENTER, 1);
+
+		this.sourceY = new Spinner(this, SWT.NONE);
+		setSpinner(this.sourceY, 2, 100, 0);
+		setGridData(this.sourceY, SWT.LEAD, SWT.CENTER, 1);
+
+		this.sourceZ = new Spinner(this, SWT.NONE);
+		setSpinner(this.sourceZ, 2, 100, 0);
+		setGridData(this.sourceZ, SWT.LEAD, SWT.CENTER, 1);
 
 		// Empty space to fit layout...
 
-		label = new Label(this, SWT.NONE);
-		label.setText("         ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("         ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		label = new Label(this, SWT.NONE);
-		label.setText("         ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 3);
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("         ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 3);
 
 		// Speed of Sound
 
-		label = new Label(this, SWT.NONE);
-		label.setText("Speed of sound (m/s): ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("Speed of sound (m/s): ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		soundSpeed = new Text(this, SWT.NONE);
-		soundSpeed.setText("344.00"); // velocidade padrao do som
-		setGridData(soundSpeed, SWT.LEAD, SWT.CENTER, 1, 40);
+		this.soundSpeed = new Text(this, SWT.NONE);
+		this.soundSpeed.setText("344.00"); // velocidade padrao do som
+		setGridData(this.soundSpeed, SWT.LEAD, SWT.CENTER, 1, 40);
 
 		// spherical receiver position
 
-		label = new Label(this, SWT.NONE);
-		label.setText("Spherical Receiver: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("Spherical Receiver: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		receiverX = new Spinner(this, SWT.NONE);
-		setSpinner(receiverX, 2, 100, 0);
-		setGridData(receiverX, SWT.LEAD, SWT.CENTER, 1);
+		this.receiverX = new Spinner(this, SWT.NONE);
+		setSpinner(this.receiverX, 2, 100, 0);
+		setGridData(this.receiverX, SWT.LEAD, SWT.CENTER, 1);
 
-		receiverY = new Spinner(this, SWT.NONE);
-		setSpinner(receiverY, 2, 100, 0);
-		setGridData(receiverY, SWT.LEAD, SWT.CENTER, 1);
+		this.receiverY = new Spinner(this, SWT.NONE);
+		setSpinner(this.receiverY, 2, 100, 0);
+		setGridData(this.receiverY, SWT.LEAD, SWT.CENTER, 1);
 
-		receiverZ = new Spinner(this, SWT.NONE);
-		setSpinner(receiverZ, 2, 100, 0);
-		setGridData(receiverZ, SWT.LEAD, SWT.CENTER, 5);
+		this.receiverZ = new Spinner(this, SWT.NONE);
+		setSpinner(this.receiverZ, 2, 100, 0);
+		setGridData(this.receiverZ, SWT.LEAD, SWT.CENTER, 5);
 
-		length.addModifyListener(new RoomSizeModifyListener(length, sourceY, receiverY));
-		width.addModifyListener(new RoomSizeModifyListener(width, sourceX, receiverX));
-		height.addModifyListener(new RoomSizeModifyListener(height, sourceZ, receiverZ));
+		this.length.addModifyListener(new RoomSizeModifyListener(this.length,
+				this.sourceY, this.receiverY));
+		this.width.addModifyListener(new RoomSizeModifyListener(this.width,
+				this.sourceX, this.receiverX));
+		this.height.addModifyListener(new RoomSizeModifyListener(this.height,
+				this.sourceZ, this.receiverZ));
 		// Sound's atenuation on air
 
-		label = new Label(this, SWT.NONE);
-		label.setText("Sound's atenuation on air: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("Sound's atenuation on air: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		soundAtenuation = new Text(this, SWT.NONE);
-		soundAtenuation.setText("0.01"); // default value
-		setGridData(soundAtenuation, SWT.LEAD, SWT.CENTER, 1, 40);
+		this.soundAtenuation = new Text(this, SWT.NONE);
+		this.soundAtenuation.setText("0.01"); // default value
+		setGridData(this.soundAtenuation, SWT.LEAD, SWT.CENTER, 1, 40);
 
 		// Espherical receiver's radius
 
-		label = new Label(this, SWT.NONE);
-		label.setText("Esferic receiver's radius: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("Esferic receiver's radius: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		radius = new Spinner(this, SWT.NONE);
-		setSpinner(radius, 2, 50, 0);
-		setGridData(radius, SWT.LEAD, SWT.CENTER, 7, 40);
+		this.radius = new Spinner(this, SWT.NONE);
+		setSpinner(this.radius, 2, 50, 0);
+		setGridData(this.radius, SWT.LEAD, SWT.CENTER, 7, 40);
 
 		// Number of rays
-		label = new Label(this, SWT.NONE);
-		label.setText("Number of rays: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(this, SWT.NONE);
+		this.label.setText("Number of rays: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		rays = new Spinner(this, SWT.NONE);
-		setSpinner(rays, 0, Integer.MAX_VALUE, 0);
-		setGridData(rays, SWT.LEAD, SWT.CENTER, 9, 40);
+		this.rays = new Spinner(this, SWT.NONE);
+		setSpinner(this.rays, 0, Integer.MAX_VALUE, 0);
+		setGridData(this.rays, SWT.LEAD, SWT.CENTER, 9, 40);
 
-				
 		// Coeficients
 		Group coefficients = new Group(this, SWT.None);
 		coefficients.setText("Acoustic Coefficients");
-		coefficients.setLayout(new GridLayout(2, false));
+		coefficients.setLayout(new GridLayout(6, false));
 		GridData coefficientsGrid = new GridData(GridData.FILL_HORIZONTAL);
 		coefficientsGrid.horizontalSpan = 6;
 		coefficients.setLayoutData(coefficientsGrid);
-		label = new Label(coefficients, SWT.NONE);
-		label.setText("Floor coefficient: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(coefficients, SWT.NONE);
+		this.label.setText("Floor coefficient: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		floorCoeficient = new Spinner(coefficients, SWT.NONE);
-		setSpinner(floorCoeficient, 2, 100, 0);
-		setGridData(floorCoeficient, SWT.LEAD, SWT.CENTER, 1, 40);
+		this.floorCoeficient = new Spinner(coefficients, SWT.NONE);
+		setSpinner(this.floorCoeficient, 2, 100, 0);
+		setGridData(this.floorCoeficient, SWT.LEAD, SWT.CENTER, 1, 40);
 
-		label = new Label(coefficients, SWT.NONE);
-		label.setText("Ceil Coefficient: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(coefficients, SWT.NONE);
+		this.label.setText("Ceil Coefficient: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		ceilCoeficient = new Spinner(coefficients, SWT.NONE);
-		setSpinner(ceilCoeficient, 2, 100, 0);
-		setGridData(ceilCoeficient, SWT.LEAD, SWT.CENTER, 1, 40);
+		this.ceilCoeficient = new Spinner(coefficients, SWT.NONE);
+		setSpinner(this.ceilCoeficient, 2, 100, 0);
+		setGridData(this.ceilCoeficient, SWT.LEAD, SWT.CENTER, 1, 40);
 
-		label = new Label(coefficients, SWT.NONE);
-		label.setText("Walls Coefficients: ");
-		setGridData(label, SWT.LEAD, SWT.CENTER, 1);
+		this.label = new Label(coefficients, SWT.NONE);
+		this.label.setText("Walls Coefficients: ");
+		setGridData(this.label, SWT.LEAD, SWT.CENTER, 1);
 
-		wallsCoeficients = new Spinner(coefficients, SWT.NONE);
-		setSpinner(wallsCoeficients, 2, 100, 0);
-		setGridData(wallsCoeficients, SWT.LEAD, SWT.CENTER, 1, 40);
+		this.wallsCoeficients = new Spinner(coefficients, SWT.NONE);
+		setSpinner(this.wallsCoeficients, 2, 100, 0);
+		setGridData(this.wallsCoeficients, SWT.LEAD, SWT.CENTER, 1, 40);
 
 		// Button that trigger the algorithm
-		compute = new Button(this, SWT.NONE);
-		compute.setText("&Compute");
-		setGridData(compute, SWT.LEAD, SWT.CENTER, 1);
+		this.compute = new Button(this, SWT.NONE);
+		this.compute.setText("&Compute");
+		setGridData(this.compute, SWT.LEAD, SWT.CENTER, 1);
 
-		compute.addSelectionListener(new SelectionAdapter() {
+		this.compute.addSelectionListener(new SelectionAdapter() {
+			@Override
 			public void widgetSelected(SelectionEvent event) {
 				try {
 					compute();
@@ -303,8 +308,8 @@ public class RayTracing extends Composite {
 			}
 		});
 
-		chart = new ChartComposite(this, SWT.NONE);
-		setGridData(chart, SWT.LEAD, SWT.BOTTOM, 10, 800, 400);
+		this.chart = new ChartComposite(this, SWT.NONE);
+		setGridData(this.chart, SWT.LEAD, SWT.BOTTOM, 10, 800, 400);
 		this.pack();
 	}
 
@@ -341,20 +346,21 @@ public class RayTracing extends Composite {
 	}
 
 	public void validate() {
-		_input.setBackground(new Color(null, 245, 245, 220));
+		this._input.setBackground(new Color(null, 245, 245, 220));
 	}
 
 	public void compute() {
 
 		List<NormalSector> sectors = generateSectorsFor();
-		List<Triade> vectors = new RandomAcousticSource().generate(rays
+		List<Triade> vectors = new RandomAcousticSource().generate(this.rays
 				.getSelection());
-		Triade soundSourceCenter = newTriadeFor(sourceX, sourceY, sourceZ);
-		Triade sphericalReceptorCenter = newTriadeFor(receiverX, receiverY,
-				receiverZ);
-		double sphericalReceptorRadius = getValue(radius);
+		Triade soundSourceCenter = newTriadeFor(this.sourceX, this.sourceY,
+				this.sourceZ);
+		Triade sphericalReceptorCenter = newTriadeFor(this.receiverX,
+				this.receiverY, this.receiverZ);
+		double sphericalReceptorRadius = getValue(this.radius);
 		double soundSpeed = Double.valueOf(this.soundSpeed.getText());
-		double mCoeficient = Double.valueOf(soundAtenuation.getText());
+		double mCoeficient = Double.valueOf(this.soundAtenuation.getText());
 		RayTracingSimulation simulation = new RayTracingSimulation(sectors,
 				vectors, soundSourceCenter, sphericalReceptorCenter,
 				sphericalReceptorRadius, soundSpeed, INITIAL_ENERGY,
@@ -364,29 +370,29 @@ public class RayTracing extends Composite {
 		Map<Double, Double> histogram = simulation
 				.getSphericalReceptorHistogram();
 		ChartBuilder builder = new ChartBuilder(histogram);
-		chart.setChart(builder.getChart());
-		chart.forceRedraw();
+		this.chart.setChart(builder.getChart());
+		this.chart.forceRedraw();
 		this.pack();
 	}
 
 	private List<NormalSector> generateSectorsFor() {
 
 		ArrayList<NormalSector> result = new ArrayList<NormalSector>();
-		double w = getValue(width);
-		double h = getValue(height);
-		double l = getValue(length);
+		double w = getValue(this.width);
+		double h = getValue(this.height);
+		double l = getValue(this.length);
 		result.add(new NormalSector(new Triade(0, 0, 1), new Triade(l, w, 0),
-				getValue(floorCoeficient)));
+				getValue(this.floorCoeficient)));
 		result.add(new NormalSector(new Triade(0, 1, 0), new Triade(l, 0, h),
-				getValue(wallsCoeficients)));
+				getValue(this.wallsCoeficients)));
 		result.add(new NormalSector(new Triade(1, 0, 0), new Triade(0, w, h),
-				getValue(wallsCoeficients)));
+				getValue(this.wallsCoeficients)));
 		result.add(new NormalSector(new Triade(0, 0, -1), new Triade(l, w, h),
-				getValue(ceilCoeficient)));
+				getValue(this.ceilCoeficient)));
 		result.add(new NormalSector(new Triade(0, -1, 0), new Triade(l, w, h),
-				getValue(wallsCoeficients)));
+				getValue(this.wallsCoeficients)));
 		result.add(new NormalSector(new Triade(-1, 0, 0), new Triade(l, w, h),
-				getValue(wallsCoeficients)));
+				getValue(this.wallsCoeficients)));
 		return result;
 	}
 
@@ -402,9 +408,8 @@ public class RayTracing extends Composite {
 		if (sourceX != null) {
 			double base = Math.pow(10, sourceX.getDigits());
 			ret = sourceX.getSelection() / base;
-		} else {
+		} else
 			ret = 0.0;
-		}
 
 		return ret;
 	}
@@ -432,20 +437,18 @@ public class RayTracing extends Composite {
 				resp.setNumraios(1);
 				resp.setProx(aux);
 			}
+		} else if ((aux != null) && (indice == aux.getIndice())) {
+			aux.setEnergia(resp.getEnergia() + energia);
+			aux.setNumraios(resp.getNumraios() + 1);
 		} else {
-			if ((aux != null) && (indice == aux.getIndice())) {
-				aux.setEnergia(resp.getEnergia() + energia);
-				aux.setNumraios(resp.getNumraios() + 1);
-			} else {
-				Response aux3;
-				aux = new Response();
-				aux3 = aux;
-				aux.setEnergia(energia);
-				aux.setIndice(indice);
-				aux.setNumraios(1);
-				aux.setProx(aux3);
-				aux2.setProx(aux);
-			}
+			Response aux3;
+			aux = new Response();
+			aux3 = aux;
+			aux.setEnergia(energia);
+			aux.setIndice(indice);
+			aux.setNumraios(1);
+			aux.setProx(aux3);
+			aux2.setProx(aux);
 		}
 	}
 
