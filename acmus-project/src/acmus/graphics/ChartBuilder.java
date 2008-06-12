0be2d9ae3a9a453798a129467feac75ef02110ce
caueguerra <>
2008-06-12 13:00:22 +0000

    novo grafico

--- acmus-project/src/acmus/graphics/ChartBuilder.java
+++ acmus-project/src/acmus/graphics/ChartBuilder.java
@@ -1,37 +1,35 @@
 package acmus.graphics;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class ChartBuilder {
 
-	private XYDataset dataset;
+	private DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 	private PlotOrientation orientation;
 	private JFreeChart chart;
-	private XYSeries series = new XYSeries("Serie");
 
 	public ChartBuilder(Map<Double, Double> map) {
+		map = new TreeMap<Double, Double>(map);
 
 		for (Map.Entry<Double, Double> e : map.entrySet())
-			this.series.add(e.getKey(), e.getValue());
-		this.dataset = new XYSeriesCollection(this.series);
+			dataset.setValue(e.getValue(), "energy", e.getKey());
 
-		this.orientation = PlotOrientation.VERTICAL;
+		orientation = PlotOrientation.VERTICAL;
 	}
 
 	public JFreeChart getChart(String x, String y, String title) {
+		chart = ChartFactory.createBarChart(title, x, y, dataset, orientation,
+				false, false, false);
 
-		this.chart = ChartFactory.createXYLineChart(title, x, y, this.dataset,
-				this.orientation, false, false, false);
-		this.chart.getPlot().setForegroundAlpha(0.5f);
+		chart.getPlot().setForegroundAlpha(0.5f);
 
-		return this.chart;
+		return chart;
 
 	}
 }
