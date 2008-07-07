1dd3c5029139eb2e6a26738a1d8e811b936810d0
caueguerra <>
2008-07-07 13:02:45 +0000

    Font as NULL! At Mac OS it wasn't working with SANS_SERIF

--- acmus-project/src/acmus/graphics/ChartBuilder.java
+++ acmus-project/src/acmus/graphics/ChartBuilder.java
@@ -6,8 +6,6 @@ import java.util.Vector;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.Axis;
-import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.xy.IntervalXYDataset;
@@ -16,77 +14,78 @@ import org.jfree.data.xy.XYIntervalSeriesCollection;
 
 public class ChartBuilder {
 
-	private IntervalXYDataset createDataset(Map<Double, Double> map) {
-		XYIntervalSeriesCollection dataset = new XYIntervalSeriesCollection();
-		XYIntervalSeries series = new XYIntervalSeries("energy");
+    private IntervalXYDataset createDataset(Map<Double, Double> map) {
+	XYIntervalSeriesCollection dataset = new XYIntervalSeriesCollection();
+	XYIntervalSeries series = new XYIntervalSeries("energy");
 
-		for (Map.Entry<Double, Double> e : map.entrySet())
-			series.add(e.getKey(), e.getKey(), e.getKey(), e.getValue(), e
-					.getValue(), e.getValue());
-
-		dataset.addSeries(series);
-		return dataset;
+	for (Map.Entry<Double, Double> e : map.entrySet()) {
+	    series.add(e.getKey(), e.getKey(), e.getKey(), e.getValue(), e
+		    .getValue(), e.getValue());
 	}
 
-	public JFreeChart getChart(Map<Double, Double> map, String x, String y,
-			String title) {
-		Font labelFont = new Font(Font.SANS_SERIF, Font.BOLD, 18);
-		Font tickFont = new Font(Font.SANS_SERIF, Font.PLAIN, 15);
-		
-		JFreeChart chart = ChartFactory.createHistogram(title, x, y,
-				createDataset(map), PlotOrientation.VERTICAL, false, false,
-				false);
+	dataset.addSeries(series);
+	return dataset;
+    }
 
-		// Setting the fonts for the axis
-		chart.getXYPlot().getRangeAxis().setLabelFont(labelFont);
-		chart.getXYPlot().getRangeAxis().setTickLabelFont(tickFont);
-		chart.getXYPlot().getDomainAxis().setLabelFont(labelFont);
-		chart.getXYPlot().getDomainAxis().setTickLabelFont(tickFont);
-		
-		return chart;
+    public JFreeChart getChart(Map<Double, Double> map, String x, String y,
+	    String title) {
+	Font labelFont = new Font(null, Font.BOLD, 18);
+	Font tickFont = new Font(null, Font.PLAIN, 15);
+
+	JFreeChart chart = ChartFactory.createHistogram(title, x, y,
+		createDataset(map), PlotOrientation.VERTICAL, false, false,
+		false);
+
+	// Setting the fonts for the axis
+	chart.getXYPlot().getRangeAxis().setLabelFont(labelFont);
+	chart.getXYPlot().getRangeAxis().setTickLabelFont(tickFont);
+	chart.getXYPlot().getDomainAxis().setLabelFont(labelFont);
+	chart.getXYPlot().getDomainAxis().setTickLabelFont(tickFont);
+
+	return chart;
+    }
+
+    public HistogramBuilder getHistogram() {
+	return new HistogramBuilder();
+    }
+
+    public class HistogramBuilder {
+	private static final int DEFAULT_BINS = 300;
+	private final HistogramDataset dataset;
+	private String title;
+	private String xLabel;
+	private String yLabel;
+
+	public HistogramBuilder() {
+	    dataset = new HistogramDataset();
+	    title = "";
+	    xLabel = "";
+	    yLabel = "";
 	}
 
-	public HistogramBuilder getHistogram() {
-		return new HistogramBuilder();
+	public HistogramBuilder addData(Vector<Double> data, String title) {
+	    double[] points = new double[data.size()];
+	    for (int i = 0; i < points.length; i++) {
+		points[i] = data.get(i);
+	    }
+	    dataset.addSeries(title, points, DEFAULT_BINS);
+	    return this;
 	}
 
-	public class HistogramBuilder {
-		private static final int DEFAULT_BINS = 300;
-		private HistogramDataset dataset;
-		private String title;
-		private String xLabel;
-		private String yLabel;
-
-		public HistogramBuilder() {
-			dataset = new HistogramDataset();
-			title = "";
-			xLabel = "";
-			yLabel = "";
-		}
-
-		public HistogramBuilder addData(Vector<Double> data, String title) {
-			double[] points = new double[data.size()];
-			for (int i = 0; i < points.length; i++) {
-				points[i] = data.get(i);
-			}
-			dataset.addSeries(title, points, DEFAULT_BINS);
-			return this;
-		}
-
-		public HistogramBuilder setTitle(String title) {
-			this.title = title;
-			return this;
-		}
-
-		public HistogramBuilder setAxisLabels(String xLabel, String yLabel) {
-			this.xLabel = xLabel;
-			this.yLabel = yLabel;
-			return this;
-		}
-
-		public JFreeChart build() {
-			return ChartFactory.createHistogram(title, xLabel, yLabel, dataset,
-					PlotOrientation.VERTICAL, true, false, false);
-		}
+	public HistogramBuilder setTitle(String title) {
+	    this.title = title;
+	    return this;
 	}
+
+	public HistogramBuilder setAxisLabels(String xLabel, String yLabel) {
+	    this.xLabel = xLabel;
+	    this.yLabel = yLabel;
+	    return this;
+	}
+
+	public JFreeChart build() {
+	    return ChartFactory.createHistogram(title, xLabel, yLabel, dataset,
+		    PlotOrientation.VERTICAL, true, false, false);
+	}
+    }
 }
