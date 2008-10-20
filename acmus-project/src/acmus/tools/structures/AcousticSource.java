e14996a2d6de01601ae94ed05cd87ca4650a4801
mariotorres1 <>
2008-10-20 12:18:11 +0000

    reorganizing directories

---
+++ acmus-project/src/acmus/tools/structures/AcousticSource.java
@@ -0,0 +1,15 @@
+package acmus.tools.structures;
+
+import java.util.List;
+
+
+/**
+ * This interface is used to provide a uniform way to RayTracing use the
+ * Strategy method, to receive generated points
+ * 
+ * @author mahtorres
+ * 
+ */
+public interface AcousticSource {
+	public List<Vector> generate(int n);
+}
