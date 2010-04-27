8c8bf6ab5c52490c0523546f27f751dc2c83cfc2
Miguel Mendes Ruiz <migmruiz@gmail.com>
2010-04-27 08:06:42 -0300

    Better usage of ConcurrentHashMap

--- acmus-project/src/acmus/simulation/structures/EnergeticSimulatedImpulseResponse.java
+++ acmus-project/src/acmus/simulation/structures/EnergeticSimulatedImpulseResponse.java
@@ -3,21 +3,22 @@ package acmus.simulation.structures;
 import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import acmus.simulation.SimulatedImpulseResponse;
 
 public final class EnergeticSimulatedImpulseResponse implements SimulatedImpulseResponse {
 
-	private Map<Integer, Float> impulseResposeHistogram;
-	// private float[] impulseResposeHistogramArray;
+	private ConcurrentHashMap<Integer, Float> impulseResposeHistogram;
 	private float interval;
+	// private float[] impulseResposeHistogramArray;
 	
 	public EnergeticSimulatedImpulseResponse(float interval) {
 		if(interval < 0.0000001)
 			throw new InvalidParameterException("interval CAN'T be less than 0.0000001");
 		
 		this.interval = interval;
-		impulseResposeHistogram = new HashMap<Integer, Float>();
+		impulseResposeHistogram = new ConcurrentHashMap<Integer, Float>();
 		
 		// impulseResposeHistogramArray = new float[188200]; // to 44100 hz;
 	}
@@ -30,7 +31,7 @@ public final class EnergeticSimulatedImpulseResponse implements SimulatedImpulse
 		return ir;
 	}
 	
-	public synchronized void addValue(float time, float energy) {
+	public void addValue(float time, float energy) {
 		if (time < 0.0f || energy < 0.0f) {
 			if (time < 0.0f) {
 				throw new InvalidParameterException("time less than ZERO");
@@ -41,11 +42,13 @@ public final class EnergeticSimulatedImpulseResponse implements SimulatedImpulse
 		
 		int position = (int) Math.ceil(time/interval);
 
-		Float storedEnergy = 0.0f;
-		if ((storedEnergy = impulseResposeHistogram.get(position)) == null) {
-			impulseResposeHistogram.put(position, energy);
-		} else {
-			impulseResposeHistogram.put(position, storedEnergy + energy);
+		Float storedEnergy;
+		if ((storedEnergy = impulseResposeHistogram.putIfAbsent(position,
+				energy)) != null) {
+			while (!impulseResposeHistogram.replace(position, storedEnergy,
+					storedEnergy + energy)){
+				storedEnergy = impulseResposeHistogram.get(position);
+			}
 		}
 		
 		// Check before re-implement: ArrayIndexOutOfBoundsException
